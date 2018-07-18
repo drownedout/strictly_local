d2a7f51432ae97682252513cb5e3034d7d0b51f8
drownedout <danielcdonnelly@gmail.com>
2018-07-18 13:27:42 -0700

    Wired redux store, reducer scaffolding

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App';
 
+import reducers from './reducers';
+
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Provider store={createStore(reducers, {})}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.querySelector('#root')
 );
---
+++ client/src/reducers/authentication.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = {
+	authenticated: '',
+	erroMessage: ''
+};
+
+export default function(state = INITIAL_STATE, action){
+	return state;
+}
