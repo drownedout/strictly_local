d0082bdaef2df748d062e6626f71ed7f1ae26106
drownedout <danielcdonnelly@gmail.com>
2018-07-18 11:47:55 -0700

    Added authentication helpers to index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,12 @@
 const Authentication = require('../controllers/authentication_controller');
+const passportService = require('../services/passport');
+const passport = require('passport');
+
+// When a user is authenticated, do not try to create a session for them
+const requireAuth = passport.authenticate('jwt', { session: false })
 
 module.exports = function(app){
-	app.get('/', function(req, res, next){
+	app.get('/', requireAuth, function(req, res, next){
 		res.send('success')
 	});
 	app.post('/signup', Authentication.signup);
