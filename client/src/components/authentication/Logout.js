45d48dc69f1361fdda503f487506abd803025191
drownedout <danielcdonnelly@gmail.com>
2018-07-19 13:25:03 -0700

    Added Logout component, functionality

---
+++ client/src/components/authentication/Logout.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Logout extends Component {
+
+	componentDidMount() {
+		this.props.logout();
+	}
+
+	render(){
+		return (
+			<div>
+				<h2>
+					You are now logged out!
+				</h2>
+			</div>
+		)
+	}
+}
+
+export default connect(null, actions)(Logout);
