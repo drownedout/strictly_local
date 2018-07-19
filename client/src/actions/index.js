f7fea2c28732ec9e6a5730ff9679c3480a55953a
drownedout <danielcdonnelly@gmail.com>
2018-07-19 12:29:31 -0700

    Added redirect for successful signup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 import { AUTH_USER, AUTH_ERROR } from './types';
 
-export const signup = formProps => async dispatch => {
+export const signup = (formProps, callback) => async dispatch => {
 	try {
-		const response = await axios.post('http://localhost:4000/signup', formProps);
+		const response = await axios.post(
+			'http://localhost:4000/signup', 
+			formProps
+		);
+
 		dispatch({ type: AUTH_USER, payload: response.data.token });
+		callback();
 	} catch (e) {
 		dispatch({ type: AUTH_ERROR, payload: 'Email in use' });
 	}
