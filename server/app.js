73ad9410bfb9bcc435b7be0266c4655a707e2ae9
drownedout <danielcdonnelly@gmail.com>
2018-08-09 14:29:54 -0700

    Refactored app

---
+++ server/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const keys = require('./config/keys');
+
+const app = express();
+const router = require('./routes/index');
+
+/** 
+	
+	Todo: 
+		Possibly refactor MongoDB keys
+		Update cors
+
+**/
+
+// DB Setup
+mongoose.connect(keys.mongoDev, (error) => {
+	if(error){
+		console.error('There was an error with Mongo DB');
+		throw error;
+	} else {
+		console.log('MongoDB has connected to the server');
+	}
+});
+
+/** App Setup **/
+
+// For logging incoming requests
+app.use(morgan('combined'));
+
+// CORS
+app.use(cors());
+
+// To parse requests into JSON
+app.use(bodyParser.json({type: '*/*'}));
+
+router(app);
+
+// Server Setup
+
+// If port is already defined, use that - otherwise, use port 3000
+
+module.exports = app;
