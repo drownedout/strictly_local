3f0ec3d94a8d8945f531cefd050e6a4662c5da84
drownedout <danielcdonnelly@gmail.com>
2018-08-10 15:27:18 -0700

    Added form styling, redirects

--- client/src/components/authentication/Login.js
+++ client/src/components/authentication/Login.js
@@ -9,41 +9,54 @@ class Login extends Component {
 	// Arrow function so that we don't need to bind
 	onSubmit = formProps => {
 		this.props.login(formProps, () => {
-			// this.props.history.push('/');
+			this.props.history.push('/');
 		});
 	};
+
+	redirectUser() {
+		if (this.props.authenticated) {
+			this.props.history.push('/');
+		}
+	}
 
 	render(){
 		// handleSubmit is provided by reduxForm
 		const { handleSubmit } = this.props;
 
 		return (
-			<form onSubmit={handleSubmit(this.onSubmit)}>
-				<fieldset>
-					<label>Email</label>
-					<Field
-						name="email"
-						type="text"
-						component="input"
-					/>
-				</fieldset>
-				<fieldset>
-					<label>Password</label>
-					<Field
-						name="password"
-						type="password"
-						component="input"
-					/>
-				</fieldset>
-				<div>{this.props.errorMessage}</div>
-				<button>Login</button>
-			</form>
+			<div className="form-container">
+				{this.redirectUser()}
+				<form className="form" onSubmit={handleSubmit(this.onSubmit)}>
+					<h1>Login</h1>
+					<fieldset>
+						<label>Email</label>
+						<Field
+							name="email"
+							type="text"
+							component="input"
+						/>
+					</fieldset>
+					<fieldset>
+						<label>Password</label>
+						<Field
+							name="password"
+							type="password"
+							component="input"
+						/>
+					</fieldset>
+					<div className="form-error">{this.props.errorMessage}</div>
+					<button className="btn primary">Login</button>
+				</form>
+			</div>
 		);
 	}
 }
 
 function mapStateToProps(state){
-	return { errorMessage: state.authentication.errorMessage };
+	return { 
+		errorMessage: state.authentication.errorMessage,
+		authenticated: state.authentication.authenticated
+	};
 }
 
 export default compose(
