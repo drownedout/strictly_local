0cb64177a83963934b20d1ce576b83774fb67e4a
drownedout <danielcdonnelly@gmail.com>
2018-08-11 16:56:41 -0700

    Updated styling for City components

--- client/src/components/cities/CityPage.js
+++ client/src/components/cities/CityPage.js
@@ -18,9 +18,34 @@ class CityPage extends Component {
 
 		return (
 			<div className="city-page">
+				<div className="city-banner">
+					<div className="city-banner-content">
+						<h1 className="city-name">{city.name}</h1>
+						<h3 className="city-state">California</h3>
+					</div>
+				</div>
 				<div className="city-page-container">
-					<h3 className="city-name">{city.name}</h3>
-					<p className="city-description">{city.description}</p>
+					<div className="city-detail-main">
+						<h2 className="city-about-title">About {city.name}, California</h2>
+						<p className="city-description">Lorem ipsum dolor sit amet, consectetur 
+						adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+						magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+						laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in 
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla qui officia 
+						deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste 
+						natus error sit voluptatem accusantium doloremque laudantium, totam 
+						rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+						architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam 
+						voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed 
+						quia consequuntur magni dolores eos qui ratione voluptatem 
+						sequi nesciunt</p>
+					</div>
+					<div className="city-detail-aside">
+						<div className="map-thumbnail">
+						</div>
+						<div className="city-meta">
+						</div>
+					</div>
 				</div>
 	        </div>
         )
