51bacd0c33f728a51cb296691c345ce0b52f1c57
drownedout <danielcdonnelly@gmail.com>
2018-08-11 17:06:42 -0700

    Updated Home page styling

--- client/src/components/static/Home.js
+++ client/src/components/static/Home.js
@@ -7,6 +7,14 @@ class Home extends Component {
 		return (
 			<section className="home">
 				<div className="home-banner">
+					<div className="home-banner-content">
+						<h1 className="home-title">Strictly Local</h1>
+						<p className="home-subtitle">Live like a local</p>
+						<div className="home-button-row">
+							<a className="btn primary">Explore</a>
+							<a className="btn secondary">Sign Up</a>
+						</div>
+					</div>
 				</div>
 				<h1 className="home-welcome">Welcome Home</h1>
 			</section>
